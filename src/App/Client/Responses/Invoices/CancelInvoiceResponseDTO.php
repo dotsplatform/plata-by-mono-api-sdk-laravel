19b5adf8f8690fd39ad559b1715dd30c97e995fa
Oleksandr Polosmak <olexandr.polosmak123@gmail.com>
2024-04-19 14:02:30 +0300

    Add mock clients and demo data response generators

--- src/App/Client/Responses/Invoices/CancelInvoiceResponseDTO.php
+++ src/App/Client/Responses/Invoices/CancelInvoiceResponseDTO.php
@@ -19,6 +19,23 @@ class CancelInvoiceResponseDTO extends PlataByMonoResponseDTO
 
     protected PlataByMonoDateTime $modifiedDate;
 
+    public static function fromArray(array $data): static
+    {
+        $data['createdDate'] = PlataByMonoDateTime::fromString($data['createdDate']);
+        $data['modifiedDate'] = PlataByMonoDateTime::fromString($data['modifiedDate']);
+
+        return parent::fromArray($data);
+    }
+
+    public function toArray(): array
+    {
+        $data = parent::toArray();
+        $data['createdDate'] = $this->getCreatedDate()->__toString();
+        $data['modifiedDate'] = $this->getModifiedDate()->__toString();
+
+        return $data;
+    }
+    
     public function getStatus(): CancelInvoiceStatus
     {
         return $this->status;
