80a11e6f168649dc672bc90195343776b364dc46
Oleksandr Polosmak <olexandr.polosmak123@gmail.com>
2024-05-30 15:55:37 +0300

    Made several fields as nullable

--- src/App/Client/Resources/Invoices/InvoicePaymentInfo.php
+++ src/App/Client/Resources/Invoices/InvoicePaymentInfo.php
@@ -13,70 +13,70 @@ class InvoicePaymentInfo extends DTO
 {
     protected ?string $maskedPan;
 
-    protected string $approvalCode;
+    protected ?string $approvalCode;
 
-    protected string $rrn;
+    protected ?string $rrn;
 
-    protected string $tranId;
+    protected ?string $tranId;
 
-    protected string $terminal;
+    protected ?string $terminal;
 
-    protected string $bank;
+    protected ?string $bank;
 
-    protected string $paymentSystem;
+    protected ?string $paymentSystem;
 
-    protected string $paymentMethod;
+    protected ?string $paymentMethod;
 
-    protected int $fee;
+    protected ?int $fee;
 
-    protected string $country;
+    protected ?string $country;
 
     public function getMaskedPan(): ?string
     {
         return $this->maskedPan;
     }
 
-    public function getApprovalCode(): string
+    public function getApprovalCode(): ?string
     {
         return $this->approvalCode;
     }
 
-    public function getRrn(): string
+    public function getRrn(): ?string
     {
         return $this->rrn;
     }
 
-    public function getTranId(): string
+    public function getTranId(): ?string
     {
         return $this->tranId;
     }
 
-    public function getTerminal(): string
+    public function getTerminal(): ?string
     {
         return $this->terminal;
     }
 
-    public function getBank(): string
+    public function getBank(): ?string
     {
         return $this->bank;
     }
 
-    public function getPaymentSystem(): string
+    public function getPaymentSystem(): ?string
     {
         return $this->paymentSystem;
     }
 
-    public function getPaymentMethod(): string
+    public function getPaymentMethod(): ?string
     {
         return $this->paymentMethod;
     }
 
-    public function getFee(): int
+    public function getFee(): ?int
     {
         return $this->fee;
     }
 
-    public function getCountry(): string
+    public function getCountry(): ?string
     {
         return $this->country;
     }
